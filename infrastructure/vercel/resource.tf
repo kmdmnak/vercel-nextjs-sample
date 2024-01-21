18812838fb0cbfd23364a3d829a4aff954b8553b
komada <kmdmuneaki@gmail.com>
2024-01-22 01:39:09 +0900

    Set root directory for frontend deploy (#3)

--- infrastructure/vercel/resource.tf
+++ infrastructure/vercel/resource.tf
@@ -1,9 +1,10 @@
 
 resource "vercel_project" "with_git" {
-  name = "vercel-nextjs-sample"
+  name      = "vercel-nextjs-sample"
   framework = "nextjs"
   git_repository = {
     type = "github"
     repo = "kmdmnak/vercel-nextjs-sample"
   }
+  root_directory = "frontend"
 }
