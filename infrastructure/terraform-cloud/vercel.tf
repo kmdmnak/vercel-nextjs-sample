bdb083e52aaa8a576f8d3e7751ea621e2ad9c81b
komada muneaki <kmdmuneaki@gmail.com>
2024-01-28 17:22:25 +0900

    restore vercel branch name

--- infrastructure/terraform-cloud/vercel.tf
+++ infrastructure/terraform-cloud/vercel.tf
@@ -1,10 +1,11 @@
 resource "tfe_workspace" "resource-vercel" {
-  name              = "vercel"
+  name              = format("vercel-%s", var.workspace_name)
   organization      = tfe_organization.vercel-nextjs-sample.name
   project_id        = tfe_project.vercel-nextjs-sample.id
   queue_all_runs    = false
   working_directory = "infrastructure/vercel"
   vcs_repo {
+    branch         = var.git_branch
     identifier     = "kmdmnak/vercel-nextjs-sample"
     oauth_token_id = tfe_oauth_client.github.oauth_token_id
   }
@@ -18,3 +19,12 @@ resource "tfe_variable" "vercel_api_token" {
   sensitive    = true
   workspace_id = tfe_workspace.resource-vercel.id
 }
+
+resource "tfe_variable" "git_branch" {
+  value        = var.git_branch
+  category     = "terraform"
+  key          = "git_branch"
+  sensitive    = false
+  workspace_id = tfe_workspace.resource-vercel.id
+}
+
