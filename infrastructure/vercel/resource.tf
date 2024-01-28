f9c0a7301512a18a3bf5a4664d2271ee6b1f17ff
komada <kmdmuneaki@gmail.com>
2024-01-28 16:14:14 +0900

    Fix deploy flow of infrastructure (#6)

--- infrastructure/vercel/resource.tf
+++ infrastructure/vercel/resource.tf
@@ -9,3 +9,9 @@ resource "vercel_project" "vercel-nextjs-sample" {
   }
   root_directory = "frontend"
 }
+
+resource "vercel_deployment" "dev" {
+  project_id        = vercel_project.with_git.id
+  ref               = "dev"
+  delete_on_destroy = true
+}
